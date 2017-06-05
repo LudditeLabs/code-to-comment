f1e03b68cff1a6faf7d9756def6403b66500a46e
konstantin <kvorobiev@gmail.com>
2017-06-05 16:20:05 +0300

    dataset random split refactored

--- seq2seq/utils/generate_random_dataset.py
+++ seq2seq/utils/generate_random_dataset.py
@@ -1,68 +1,68 @@
 ###########################################################################################################
 # Author: Tjalling Haije
-# Project: code-to-comment 
+# Project: code-to-comment
 # For: Bsc AI, University of Amsterdam
 # Date: May, 2016
 ###########################################################################################################
 
 import os
-import sys
-import subprocess
-import re
+import os.path as op
 import random
 
 
+base_path = "/home/konstantin/work/ludditelabs/data/"
+input_code_file = op.join(base_path, "allCodeCommentOnly/all.code")
+input_en_file = op.join(base_path, "allCodeCommentOnly/all.en")
+output_dev_en_file = op.join(base_path, "allCodeCommentOnly/dev/10pt.random.en")
+output_dev_code_file = op.join(base_path, "allCodeCommentOnly/dev/10pt.random.code")
+output_train_code_file = op.join(base_path, "allCodeCommentOnly/train/90pt.random.code")
+output_train_en_file = op.join(base_path, "allCodeCommentOnly/train/90pt.random.en")
+
+all_files = [input_code_file, input_en_file, output_dev_en_file, output_dev_code_file,
+             output_train_code_file, output_train_en_file]
+
+
 # this function adds spaces around all punctuation in the specified file, and writes it to the output file
-def gen_random_dataset(): 
-
-    input_code_file = "../data/allCodeCommentOnly/all.code"
-    input_en_file = "../data/allCodeCommentOnly/all.en"
-    output_dev_en_file = "../data/allCodeCommentOnly/dev/10pt.random.en"
-    output_dev_code_file = "../data/allCodeCommentOnly/dev/10pt.random.code"
-    output_train_code_file = "../data/allCodeCommentOnly/train/90pt.random.code"
-    output_train_en_file = "../data/allCodeCommentOnly/train/90pt.random.en"
-
-
-    # read the original files
-    with open(input_code_file) as f:
+def gen_random_dataset():
+    """ Randomly split .code and .en files into training (dev) and test files. Training file
+        contains 10% of all data, test file 90%
+    """
+    with open(input_code_file) as f, open(input_en_file) as g:
         code_file = f.readlines()
-    with open(input_en_file) as g:
         en_file = g.readlines()
 
-    fileLength = len(code_file)
-    # create a list with all lines shuffled
-    indices = random.sample(range(0, fileLength), fileLength)
+    idxs = random.suffle(range(len(code_file)))
+
+    code_file = code_file[idxs]
+    en_file = en_file[idxs]
+
+    delim_point = int(round(len(code_file) * 0.1))
 
     # open the dev files
-    with open(output_dev_en_file, 'w') as dev_en_file:
-        with open(output_dev_code_file, 'w') as dev_code_file:
-                
-            # write 10% random lines to the dev files
-            for x in xrange(0, int(round(0.1 * fileLength))):
-                dev_en_file.write(en_file[indices[x]])
-                dev_code_file.write(code_file[indices[x]])
+    with open(output_dev_en_file, 'w') as dev_en_file, open(output_dev_code_file, 'w') as dev_code_file:
+        dev_en_file.writelines(en_file[:delim_point])
+        dev_code_file.writelines(code_file[:delim_point])
 
     print ("Dev files created.")
 
     # open the train files
-    with open(output_train_en_file, 'w') as train_en_file:
-        with open(output_train_code_file, 'w') as train_code_file:
-
-            # write 90% random lines to the train files
-            for x in xrange(int(round(0.1 * fileLength)), fileLength):
-                train_en_file.write(en_file[indices[x]])
-                train_code_file.write(code_file[indices[x]])
+    with open(output_train_en_file, 'w') as train_en_file, open(output_train_code_file, 'w') as train_code_file:
+        # write 90% random lines to the train files
+        train_en_file.writelines(en_file[delim_point:])
+        train_code_file.writelines(code_file[delim_point:])
 
     print ("Train files created.")
     print ("Done.")
 
 
-# def main(_):
-#     gen_random_dataset()
-#     # os.system("python punctuation_police.py")
-
-
+def main():
+    for f in all_files:
+        d = op.dirname(f)
+        if not op.exists(d):
+            os.makedirs(d)
+    gen_random_dataset()
+    # os.system("python punctuation_police.py")
 
 
 if __name__ == "__main__":
-    gen_random_dataset()
+    main()
