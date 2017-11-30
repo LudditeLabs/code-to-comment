75d4447a61c4086ccbba821d1148c4c3d4062f66
Konstantin <kvorobiev@gmail.com>
2017-11-30 21:23:50 +0300

    WIP

--- dataset/datavisual.py
+++ dataset/datavisual.py
@@ -16,6 +16,10 @@ logging.basicConfig(
 )
 
 
+COMMENT_INDEX = 1
+CODE_INDEX = 0
+
+
 class DataVisualizer():
 
     def __init__(self, dbpath):
@@ -31,19 +35,24 @@ class DataVisualizer():
             buckets = BUCKETS
         # last bucket for all outliers
 
-        code = [p[0] for p in codecomments]
-        comments = [p[1] for p in codecomments]
+        code = [p[0] for p in data]
+        comments = [p[1] for p in data]
 
         buckets_cnt = [len(filter(lambda x: x > buckets[i-1] and x < buckets[i], data)) for i in range(1, len(buckets))]
         return buckets_cnt
 
+    def seq_lens(self, data, comment=False):
+        ind = COMMENT_INDEX if comment else CODE_INDEX
+        d = [len(p[ind]) for p in data]
+        return d
+
     def form_hist(self, **kwargs):
         """ Form a distribution histogram among buckets
         """
+        valid_hist_args = {"bins", "range"}
         codecomments = self.db.get_codecomment_pairs()
-
-
-        return buckets_cnt
+        distr = self.seq_lens(codecomments, **kwargs)
+        plt.hist()
 
 
 def main():
